477f6ee85a8eb45f342d9339e04d550f5c5b89c4
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-08-17 23:31:12 +0200

    Add Resource Entity

--- src/resources/entities/resource.entity.ts
+++ src/resources/entities/resource.entity.ts
@@ -1,1 +1,55 @@
-export class Resource {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class Resource {
+  @Column()
+  uuid: string;
+
+  @Column()
+  uuid_link: string;
+
+  @Column()
+  uuid_uri_type: string;
+
+  @PrimaryColumn()
+  uuid_rda: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  notes: string;
+
+  @Column()
+  uri: string;
+
+  @Column()
+  uri2: string;
+
+  @Column()
+  pid_lod_type: string;
+
+  @Column()
+  pid_lod: string;
+
+  @Column()
+  dc_date: string;
+
+  @Column()
+  dc_description: string;
+
+  @Column()
+  dc_language: string;
+
+  @Column()
+  type: string;
+
+  @Column()
+  dc_type: string;
+
+  @Column()
+  card_url: string;
+
+  @Column()
+  source: string;
+}
--- src/resources/resources.module.ts
+++ src/resources/resources.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ResourcesService } from './resources.service';
 import { ResourcesController } from './resources.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Resource } from './entities/resource.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Resource])],
   controllers: [ResourcesController],
   providers: [ResourcesService],
 })
