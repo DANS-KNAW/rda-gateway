e7fa94e87d10e6a5c8eadf29d213c4e358f5caac
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-08-17 23:48:01 +0200

    Add GroupGroup Entity

---
+++ src/entities/group-group.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class GroupGroup {
+  @PrimaryColumn()
+  UUID_Group1: string;
+
+  @Column()
+  Title_Group1: string;
+
+  @Column()
+  Relation: string;
+
+  @PrimaryColumn()
+  UUID_Group2: string;
+
+  @Column()
+  Title_Group2: string;
+
+  @Column()
+  Relation_Description: string;
+
+  @Column()
+  Description_source: string;
+
+  @Column()
+  Description_URL: string;
+}
