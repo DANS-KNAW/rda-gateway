98028343bf107edad809569e2da7a4ae780d9d1d
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-08-20 08:55:00 +0200

    Add Annotation Type

--- src/annotations/entities/annotation.entity.ts
+++ src/annotations/entities/annotation.entity.ts
@@ -1,1 +1,34 @@
-export class Annotation {}
+export type vocab = {
+  id: string;
+  label: string;
+  value: string;
+  description?: string;
+  url?: string;
+};
+
+export class Annotation {
+  page_url: string;
+  annotation: string;
+  uritype: string;
+  citation: {
+    title: string;
+    description?: string;
+    notes?: string;
+    submitter?: string;
+    language: {
+      id: string;
+      label: string;
+      value: string;
+    };
+    created_at: string;
+    resource?: string;
+  };
+  vocabularies: {
+    pathways?: vocab[];
+    gorc_attributes?: vocab[];
+    gorc_elements?: vocab[];
+    working_groups?: vocab[];
+    interest_groups?: vocab[];
+    domains?: vocab[];
+  };
+}
