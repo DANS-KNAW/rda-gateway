56aa2d508890210904364d8727251646a2d379a3
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-09-09 12:32:59 +0200

    fix(config): Enforced environment being required
    The validation-schema will now enforce ALL properties inside of an Zod
    object are required. Should an enviroment variable be optional ensure
    that a sensible default is set inside of the object.

--- src/config/validation-schema.ts
+++ src/config/validation-schema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 import { CoreConfigSchema } from './core.config';
 
-export const EnvironmentSchema = CoreConfigSchema;
+export const EnvironmentSchema = CoreConfigSchema.required();
 
 export type EnvironmentVariables = z.infer<typeof EnvironmentSchema>;
