b567071686c29ac48b8e7aeead2930f8f44fd0de
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2025-09-11 14:56:46 +0200

    feat(validation): setup global validation pipe
    The global validation pipe ensures that and DTO object with decorators
    will be validated. This allows us to sanitize any incoming response with
    minimal effort.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService, ConfigType } from '@nestjs/config';
+import coreConfig, { CONFIG_CORE_TOKEN } from './config/core.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const config =
+    configService.getOrThrow<ConfigType<typeof coreConfig>>(CONFIG_CORE_TOKEN);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      enableDebugMessages: config.NODE_ENV === 'development',
+    }),
+  );
+
+  await app.listen(config.API_PORT);
 }
+
 bootstrap().catch((err) => {
   console.error('Error starting the application:', err);
   process.exit(1);
